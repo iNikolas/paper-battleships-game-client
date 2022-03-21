e2535f3dffc207f5be78e9513ca5c7c0a862526e
iNikolas <realkolos1@gmail.com>
2022-03-22 01:23:33 +0200

    first commit

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,5 +1,5 @@
 const SERVER = 'paper-battleships.herokuapp.com'
 
-export const BACKEND = `http://${SERVER}`
-export const WS_URI = `ws://${SERVER}`
+export const BACKEND = `https://${SERVER}`
+export const WS_URI = `wss://${SERVER}`
 export const TIMER = {link: null}
